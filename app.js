7ee425d84c420179d937318f7e2b9bda6369a0b5
KConlon13 <KyleConlon13@gmail.com>
2020-03-12 13:09:29 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,59 @@
+
+// function sayHello(name){
+//     console.log("Hello "+ name);
+// }
+// sayHello('Kyle')
+
+// var message = 'sup';
+// console.log(message);
+// this will return undefined 
+
+// const log = require('./logger');
+
+// log('message')
+
+// const os = require('os')
+
+// var totalMemory = os.totalmem()
+// var freeMemory = os.freemem()
+
+// console.log('Total Memory: '+ totalMemory)
+
+// console.log(`Free memory: ${freeMemory}`)
+
+
+// const fs = require('fs');
+
+// fs.readdir('./', function(err, files){
+//     if (err) console.log('Error', err);
+//     else console.log('Result', files)
+// })
+
+// const EventEmitter = require('events');
+
+// const Logger = require('./logger')
+// const logger = new Logger();
+
+// logger.on('messageLogged', (arg) => {
+//     console.log('Listener called', arg)
+// })
+
+// logger.log('message')
+
+const http = require('http');
+
+const server = http.createServer((req, res)=>{
+    if (req.url === '/'){
+        res.write("Hell0 World")
+        res.end();
+    } 
+
+    if (req.url === '/api/courses'){
+        res.write(JSON.stringify([1, 2, 3]))
+        res.end();
+    }
+});
+
+server.listen(3000)
+
+console.log('Listening on port 3000...')
